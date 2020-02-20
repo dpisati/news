6bd1561a98393bb86ac4c8a7ce4762288694b1fe
Daniel Pisati <dpisati@gmail.com>
2020-02-21 11:21:29 +1300

    url

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,9 @@ import thunk from "redux-thunk";
 const middleware = applyMiddleware(thunk, logger);
 
 const initialState = {
-  searchField: "oi",
+  searchField: "Initial Search String",
+  url:
+    "http://newsapi.org/v2/top-headlines?country=nz&apiKey=fe8a84b676c84ea387ddba2872f9c0d6",
   news: [
     {
       title: "Esse eh um test",
@@ -37,7 +39,11 @@ function reducer(state = initialState, action) {
     case "SEARCH_CHANGE":
       return {
         ...state,
-        searchField: action.payload
+        searchField: action.payload,
+        url:
+          "http://newsapi.org/v2/top-headlines?q=" +
+          action.payload +
+          "&apiKey=fe8a84b676c84ea387ddba2872f9c0d6"
       };
 
     default:
