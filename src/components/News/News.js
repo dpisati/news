444a8726e9c10835b35496587eb14ade7a1351dc
Daniel Pisati <dpisati@gmail.com>
2020-02-20 14:41:44 +1300

    redux_obj

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -4,12 +4,11 @@ import NewsCard from "./NewsCard";
 
 export const News = ({ news }) => {
   if (news) {
-    console.log("NEWS", news[0].news);
     return (
       <div className="newsBody">
         <div className="articlesCards">
           <div className="cardArticle">
-            {news[0].news.map(article => (
+            {news.map(article => (
               <NewsCard key={article.title} article={article} />
             ))}
           </div>
@@ -26,13 +25,4 @@ export const News = ({ news }) => {
   }
 };
 
-// export const News = ({ news }) => {
-//   return (
-//     <div className="newsBody">
-//       {console.log("from component NEWS", news[0].news[0])}
-//       <div className="articlesCards">{renderNews(news[0].news[0])}</div>
-//     </div>
-//   );
-// };
-
-export default connect(state => ({ news: state }))(News);
+export default connect(state => ({ news: state.news }))(News);
--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,26 @@
 import { createStore } from "redux";
 
 function reducer() {
-  return [
-    {
-      news: [
-        {
-          title: "Esse eh um test",
-          description:
-            "Esse eh apenas um artigo em teste para ver se o redux funciona"
-        },
-        {
-          title: "Esse eh um test 2",
-          description:
-            "Esse eh apenas um artigo em teste para ver se o redux funciona 2"
-        },
-        {
-          title: "Esse eh um test 3",
-          description:
-            "Esse eh apenas um artigo em teste para ver se o redux funciona 3"
-        }
-      ]
-    }
-  ];
+  return {
+    searchField: "",
+    news: [
+      {
+        title: "Esse eh um test",
+        description:
+          "Esse eh apenas um artigo em teste para ver se o redux funciona"
+      },
+      {
+        title: "Esse eh um test 2",
+        description:
+          "Esse eh apenas um artigo em teste para ver se o redux funciona 2"
+      },
+      {
+        title: "Esse eh um test 3",
+        description:
+          "Esse eh apenas um artigo em teste para ver se o redux funciona 3"
+      }
+    ]
+  };
 }
 
 const store = createStore(reducer);
